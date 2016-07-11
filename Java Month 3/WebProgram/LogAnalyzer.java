28e4c5afd9915af06f6521a7f28d176d6f816e15
SSaeed <saadsaeed85@gmail.com>
2016-07-10 21:18:58 -0500

    First commit week7: logEntries

---
+++ Java Month 3/WebProgram/LogAnalyzer.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         records = new ArrayList<LogEntry>();
+     }
+        
+     public void readFile(String filename) {
+         FileResource fr = new FileResource(filename);
+         WebLogParser wParser = new WebLogParser();
+         for(String line : fr.lines()){
+            records.add(wParser.parseEntry(line));
+         }
+     }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
